534908c4396b680b9634f5c9f0bfbde2176569cc
vidityar adith <van.adith@gmail.com>
2022-05-12 13:16:20 +0700

    Start making dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,9 +1,107 @@
 <template>
-    <div>
-        <span>
-        {{ "HALAMAN DASHBOARD"}}
-        </span> 
-    </div>
+    <v-container fluid>
+        <v-row>
+            <br>
+            <v-col
+                cols="12"
+                md="12"
+            >
+            <v-icon>mdi-map-marker-radius</v-icon>
+            <span>{{"Bumi Marina Emas"}}</span>
+            </v-col>
+            <v-col
+                cols="12"
+                md="5"
+            >
+                <v-img
+                    alt="IoT Greenhouse Card"
+                    class="shrink mr-2"
+                    contain
+                    src="@/assets/card.jpg"
+                    transition="scale-transition"
+                    max-width="450px"
+                />
+            </v-col>
+            <v-col
+                cols="12"
+                md="5"
+            >
+                <v-img
+                alt="IoT Greenhouse Card"
+                class="shrink mr-2"
+                contain
+                src="@/assets/card.jpg"
+                transition="scale-transition"
+                max-width="450px"
+                />
+            </v-col>
+            <v-col
+                cols="12"
+                md="4"
+            >
+                <v-img
+                alt="IoT Greenhouse Card"
+                class="shrink mr-2"
+                contain
+                src="@/assets/card3.jpg"
+                transition="scale-transition"
+                max-width="250px"
+                />
+            </v-col>
+            <v-col
+                cols="12"
+                md="4"
+            >
+                <v-img
+                alt="IoT Greenhouse Card"
+                class="shrink mr-2"
+                contain
+                src="@/assets/card3.jpg"
+                transition="scale-transition"
+                max-width="250px"
+                />
+            </v-col>
+            <v-col
+                cols="12"
+                md="4"
+            >
+                <v-img
+                alt="IoT Greenhouse Card"
+                class="shrink mr-2"
+                contain
+                src="@/assets/card3.jpg"
+                transition="scale-transition"
+                max-width="250px"
+                />
+            </v-col>
+            <v-col
+                cols="12"
+                md="4"
+            >
+                <v-img
+                alt="IoT Greenhouse Card"
+                class="shrink mr-2"
+                contain
+                src="@/assets/KranOpen.png"
+                transition="scale-transition"
+                max-width="200px"
+                />
+            </v-col>
+            <v-col
+                cols="12"
+                md="4"
+            >
+                <v-img
+                alt="IoT Greenhouse Card"
+                class="shrink mr-2"
+                contain
+                src="@/assets/BulbOn.png"
+                transition="scale-transition"
+                max-width="200px"
+                />
+            </v-col>
+        </v-row>
+    </v-container>
     
 </template>
 
